34c08ee9c9fac3de76532614c7b39d30610cc855
Martins Abiodun <martinsabiodun94@gmail.com>
2020-03-31 18:41:15 +0100

    modified homepage banner

--- src/components/LargeBanner.vue
+++ src/components/LargeBanner.vue
@@ -1,10 +1,10 @@
 <template>
       <!-- banner area start -->
-    <div class="banner-area jarallax" style="background-image: url(/img/home.png);">
+    <div class="banner-area jarallax" style="background-image: url(/img/anibi.png);">
         <div class="container">
             <div class="banner-inner-wrap">
                 <div class="row">
-                    <!-- <div class="col-lg-10">
+                    <div class="col-lg-10">
                         <div class="banner-inner">
                             <h5 class="sub-title">Built With Sructure</h5>
                             <h1 class="title">Modern New Apartment In Residentioal Complex</h1>
@@ -12,7 +12,7 @@
                                 <a class="btn btn-yellow mr-2" href="/property">CHECK PROPERTIES</a>
                             </div>
                         </div>
-                    </div> -->
+                    </div>
                 </div>
             </div>
         </div>
