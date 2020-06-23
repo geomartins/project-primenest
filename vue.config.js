98265aba0444671990f4d19611e6731372aa2b78
Martins Abiodun <martinsabiodun94@gmail.com>
2020-06-23 15:47:47 +0100

    cache busting by hashing

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 //const manifestJSON = require('./public/manifest.json')
 module.exports = {
     chainWebpack: (config) => {
-        //if (process.env.NODE_ENV === 'development') {
-          config.plugins.delete('preload')
-         
-        //}
+      config.plugins.delete('preload')
+      config.plugin('html').tap(args => {
+        args[0].hash = true
+        return args
+      })
+        
     },
     pwa: {
-        //themeColor: manifestJSON.theme_color,
-        // name: manifestJSON.short_name,
-        // msTileColor: manifestJSON.background_color,
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
         workboxPluginMode: 'InjectManifest',
@@ -23,7 +22,6 @@ module.exports = {
       before: app => {
         app.set('etag', 'strong')
         app.use((req, res, next) => {
-            console.log('Hidddddddddddddddddd')
           res.set('Cache-Control', 'must-revalidate')
           next()
         })
